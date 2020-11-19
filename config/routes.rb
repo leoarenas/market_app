ef7b2727ac3432031e0ff03b9b41f595f3181075
Leonardo Arenas <leo_arenas@hotmail.com>
2020-11-18 22:44:34 -0300

    Cambios en el route para poder dar de alta un usuario

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     delete '/users/sign_out' => 'devise/sessions#destroy'
+    post '/users/sign_up' => 'devise/registrations#new'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
